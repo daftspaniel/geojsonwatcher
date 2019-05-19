f08eb7385a1797fbc816d8ddbf625af48b685665
Davy Mitchell <daftspaniel@gmail.com>
2019-05-19 19:33:16 +0100

    Tidy input when constructing a Feature.

--- geojsonwatcher/data_structures/feature.py
+++ geojsonwatcher/data_structures/feature.py
@@ -7,12 +7,16 @@ from geojsonwatcher.common.util import timestamp_to_time
 
 class Feature(object):
     def __init__(self, mag, time, place, url):
-        self.mag = str(mag)
+        self.mag = f'{mag:.2f}'
         self.time = timestamp_to_time(time)
         location = place.split(',')
-        self.site = location[0]
+        self.site = sanitise_text(location[0])
         if len(location) > 1:
-            self.area = location[1]
+            self.area = sanitise_text(location[1])
         else:
-            self.area = 'Unknown.'
+            self.area = ' Unknown.'
         self.url = url
+
+
+def sanitise_text(text):
+    return text.replace("'", "")
