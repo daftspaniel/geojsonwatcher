db88e11633c40d80284b0ec821372c2f01de1c0b
Davy Mitchell <daftspaniel@gmail.com>
2019-05-22 22:07:49 +0100

    Fix unknown handler.

--- geojsonwatcher/data_structures/feature.py
+++ geojsonwatcher/data_structures/feature.py
@@ -11,10 +11,7 @@ class Feature(object):
         self.time = timestamp_to_time(time)
         location = place.split(',')
         self.site = sanitise_text(location[0])
-        if len(location) > 1:
-            self.area = sanitise_text(location[1])
-        else:
-            self.area = ' Unknown.'
+        self.area = sanitise_text(location[1]) if len(location) > 1 else 'Unknown.'
         self.url = url
 
 
